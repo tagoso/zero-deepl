e0b863f56f42cb39d711a64eba7a6e245b9bf462
tagoso <shirouo4279@gmail.com>
2025-10-14 23:48:32 +0300

    Day 1

---
+++ neural-network.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+# there are many kinds of active functions!
+#
+# 3.2.1 Sigmoid Function
+#
+# h(x)=1/1+e^(-x)
+#
+
+# 3.2.2 first, redefine 0/1 logic gate as step function
+#
+
+
+def step_function_1(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
+
+
+# above, x cannot be NumPy arrays such as step_function(np.array([1.0, 2.0]))
+# so...
+
+
+def step_function_2(x):
+    y = x > 0
+    return y.astype(np.int)
+
+
+# x = np.array([-1.0, 1.0, 2.0, 0])
+# print(x)
+
+# y as boolean
+# y = x > 0
+# print(y)
+
+# y_type
+# y_t = y.astype(np.int64)
+# print(y_t)
+
+
+# 3.2.3 Graph of Step Function
+#
+# def step_function3(x):
+#     return np.array(x > 0, dtype=np.int64)
+
+
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = step_function3(x)
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)  # specify y axis range
+# plt.show()
+
+
+# 3.2.4 Here is finally Sigmoid Function!
+#
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+x_sigmoid = np.array([-1.0, 1.0, 2.0])
+print(sigmoid(x_sigmoid))
+
+t = np.array([1.0, 2.0, 3.0])
+print(1.0 + t)
+print(1.0 / t)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)  # specify y axis range
+plt.show()
+
+
+# 3.2.7 ReLU Function
+#
+def relu(x):
+    return np.maximum(0, x)
+
+
+# 3.3 Multi-Dimensional Arrays 🔥
+#
