2f57bc8472c65d51c8eacbe62b14d39b157707cb
tagoso <shirouo4279@gmail.com>
2025-10-17 23:56:41 +0300

    Affine

--- 3_neural_network_3.py
+++ 3_neural_network_3.py
@@ -71,8 +71,8 @@ def predict(network, x):
     W1, W2, W3 = network["W1"], network["W2"], network["W3"]
     b1, b2, b3 = network["b1"], network["b2"], network["b3"]
 
-    a1 = np.dot(x, W1) + b1
-    z1 = sigmoid(a1)
+    a1 = np.dot(x, W1) + b1  # a is Affine's a.
+    z1 = sigmoid(a1)  # z is result of activation.
     a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
